bd4d5d4d955d5a6a35944b8c31a439bfc911f9a3
fox <fox@fox.fox>
2019-01-08 00:46:24 +0300

    Music store handlebars local and settings

--- main.js
+++ main.js
@@ -33,6 +33,11 @@ const context = {
 
 const templateElement = document.getElementById("templateHB");
 
-const templateSource = templateElement.innerHTML
+const templateSource = templateElement.innerHTML;
 
 const template = Handlebars.compile(templateSource);
+
+const compiledHtml = template(context);
+
+document.getElementById("information").innerHTML = compiledHtml;
+
